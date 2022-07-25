6497e1b07eca7ba56b03089b71ebb26871485eb0
Sandi Sopian <shunasand73@gmail.com>
2022-07-25 13:30:29 +0700

    hero section feature

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
           <button className="border-none bg-transparent text-black mr-4">Sign In</button>
           <button className="px-8 py-3">Sign Up</button>
         </div>
-        <div className="md:hidden" onClick={handleClick}>
+        <div className="md:hidden hover:cursor-pointer" onClick={handleClick}>
           {!nav ? <MenuIcon className="w-5" /> : <XIcon className="w-5" />}
         </div>
       </div>
